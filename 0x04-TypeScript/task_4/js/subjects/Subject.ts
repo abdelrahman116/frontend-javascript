b6593ad7c2da76147db27d5494ec69c90a993703
Abdelrahman Dawoud <112118879+abdelrahman116@users.noreply.github.com>
2025-11-12 13:18:47 +0200

    Refactor setTeacher method formatting

--- 0x04-TypeScript/task_4/js/subjects/Subject.ts
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -1,7 +1,7 @@
 namespace Subjects {
   export class Subject {
     teacher: Teacher;
-    setTeacher(teacher: Teacher) {
+    setTeacher(teacher:Teacher) {
       this.teacher = teacher;
     }
   }
