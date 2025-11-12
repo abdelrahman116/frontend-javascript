d914bd456f9f066c9483a1c1b800ca017b84c266
Abdelrahman Dawoud <abdelrahmandawoud11@gmail.com>
2025-11-12 14:01:41 +0200

    10

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -1,25 +1,27 @@
-export const cpp = new Subjects.Cpp();
-export const java = new Subjects.Java();
-export const react = new Subjects.React();
-export const cTeacher: Subjects.Teacher = {
-  firstName: "John",
-  lastName: "Doe",
-  experienceTeachingC: 10,
-};
-cpp.setTeacher(cTeacher);
-const cppRequirements = cpp.getRequirements();
-const cppAvailableTeacher = cpp.getAvailableTeacher();
-console.log(cppRequirements);
-console.log(cppAvailableTeacher);
+namespace Subjects {
+  export const cpp = new Cpp();
+  export const java = new Java();
+  export const react = new React();
+  export const cTeacher: Subjects.Teacher = {
+    firstName: "John",
+    lastName: "Doe",
+    experienceTeachingC: 10,
+  };
+  cpp.setTeacher(cTeacher);
+  const cppRequirements = cpp.getRequirements();
+  const cppAvailableTeacher = cpp.getAvailableTeacher();
+  console.log(cppRequirements);
+  console.log(cppAvailableTeacher);
 
-java.setTeacher(cTeacher);
-const javaRequirements = java.getRequirements();
-const javaAvailableTeacher = java.getAvailableTeacher();
-console.log(javaRequirements);
-console.log(javaAvailableTeacher);
+  java.setTeacher(cTeacher);
+  const javaRequirements = java.getRequirements();
+  const javaAvailableTeacher = java.getAvailableTeacher();
+  console.log(javaRequirements);
+  console.log(javaAvailableTeacher);
 
-react.setTeacher(cTeacher);
-const reactRequirements = react.getRequirements();
-const reactAvailableTeacher = react.getAvailableTeacher();
-console.log(reactRequirements);
-console.log(reactAvailableTeacher);
+  react.setTeacher(cTeacher);
+  const reactRequirements = react.getRequirements();
+  const reactAvailableTeacher = react.getAvailableTeacher();
+  console.log(reactRequirements);
+  console.log(reactAvailableTeacher);
+}
